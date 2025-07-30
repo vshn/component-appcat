3a47fa229ea137ff1b2ea360e40deb8e5a4a04d8
Simon Beck <simon.beck@vshn.ch>
2025-07-30 11:28:12 +0000

    Fix e2e connection job assertion
    Somewhere between k8s 1.29 and 1.31 jobs received a new condition
    c`type `SuccessCriteriaMet` see https://kubernetes.io/docs/concepts/workloads/controllers/job/#success-policy.
    
    However this new condition confused our Kuttl tests as it expected
    a condition with type `Complete` as the first element of the array.
    
    Also the deletion protection is now only enabled during the actual
    test. Otherwise it's disabled, this prevents left over instances, if
    the tests fail for some reason before the deletion protection gets
    removed.

--- tests/e2e/common/scripts/check-protection.sh
+++ tests/e2e/common/scripts/check-protection.sh
@@ -7,12 +7,16 @@ name="$2"
 
 ns=$(kubectl -n "$NAMESPACE" get "$type" "$name" -oyaml | yq -r '.status.instanceNamespace')
 
+kubectl -n "$NAMESPACE" patch "$type" "$name" -p '{"spec":{"parameters":{"security":{"deletionProtection": true}}}}' --type merge
+
 if ! kubectl -n "$NAMESPACE" delete "$type" "$name" ; then
   echo "instance protected"
 else
   echo "instance got deleted! Please check deletion protection!"
     false
 fi
+
+kubectl -n "$NAMESPACE" patch "$type" "$name" -p '{"spec":{"parameters":{"security":{"deletionProtection": false}}}}' --type merge
 
 if ! kubectl delete ns "$ns" ; then
   echo "namespace protected"
