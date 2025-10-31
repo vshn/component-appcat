6973a8c16b2bcdd2ce141eb23b74f33de05c087b
DevContainer User <devcontainer@local.dev>
2025-10-31 16:33:06 +0100

    cnpg: base e2e tests + maintenance

---
+++ tests/e2e/postgresqlcnpg/scripts/check-maintenance.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -ex
+
+NAMESPACE="${NAMESPACE:-appcat-e2e}"
+ns=$(kubectl -n "$NAMESPACE" get vshnpostgresql postgresql-e2e-test  -oyaml | yq -r '.status.instanceNamespace')
+
+# Initial maintenance should be completed
+job=$(kubectl -n "$ns" get job -o name | grep "initial-maintenance")
+kubectl -n "$ns" wait --for=condition=complete --timeout=1000s $job
+
+# Try vacuum
+vacuum_enabled=$(kubectl get -n "$ns" cronjobs.batch --ignore-not-found maintenancejob -o jsonpath='{.spec.jobTemplate.spec.template.spec.containers[0].env[?(@.name=="VACUUM_ENABLED")].value}')
+[ "$vacuum_enabled" == "true" ]
+
+while true; do
+  instance=$(kubectl -n "$ns" get cluster -o yaml | yq -r .items[0].status.readyInstances)
+  # Check that instances are >0
+  if [ "$instance" -gt 0 ]; then
+    break
+  fi
+  echo "Waiting for cluster to start reporting ready instances..."
+  sleep 10
+done
+
+kubectl -n "$ns" create job --from=cronjob/maintenancejob manual-maintenance-job
+kubectl -n "$ns" wait --for=condition=complete --timeout=1000s job/manual-maintenance-job
+
+# Check that the job is reporting "vacuuming database" in its logs
+kubectl -n "$ns" logs job/manual-maintenance-job | grep -i "vacuuming database" || (echo "Vacuuming not found in logs" && exit 1)
