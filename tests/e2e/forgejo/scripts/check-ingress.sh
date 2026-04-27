721b02995b3e1af2c12c4d399186414d341c68da
Simon Beck <simon.beck@vshn.ch>
2026-04-27 12:37:39 +0000

    Increase reliability of forgejo e2e test
    The ordering for the ingress object's host stanza doesn't seem to be
    stable between different cluster implementations. The test failed on
    Talos and seemingly never on Kind.
    
    The test was adjusted so it doesn't rely on a specific ordering of the
    hosts array.

--- tests/e2e/forgejo/scripts/check-ingress.sh
+++ tests/e2e/forgejo/scripts/check-ingress.sh
@@ -7,7 +7,7 @@ NAMESPACE="${NAMESPACE:-appcat-e2e}"
 ns=$(kubectl -n "$NAMESPACE" get vshnforgejo forgejo-e2e -oyaml | yq -r '.status.instanceNamespace')
 composite=$(kubectl -n "$NAMESPACE" get vshnforgejo forgejo-e2e -oyaml | yq -r '.spec.resourceRef.name')
 
-fqdn_sub=$(kubectl -n "$ns" get ingress "${composite}-letsencrypt-ingress"  -oyaml | yq -r '.spec.tls.[0].hosts[0]')
+fqdn_sub=$(kubectl -n "$ns" get ingress "${composite}-letsencrypt-ingress" -oyaml | yq -r '.spec.tls[].hosts[] | select(test("sub1"))')
 
 echo "$fqdn_sub = sub1.forgejo-e2e.apps.lab-cloudscale-rma-0.appuio.cloud"
 [[ "$fqdn_sub" == "sub1.forgejo-e2e.apps.lab-cloudscale-rma-0.appuio.cloud" ]]
